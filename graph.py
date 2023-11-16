d5cda31d78903b64da33dbe0795808ebf01d78a0
Ryan Moorgen <ryan8mo@gmail.com>
2023-11-16 14:53:20 -0500

    added file

---
+++ graph.py
@@ -0,0 +1,9 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+# Example Source: https://pythontic.com/visualization/charts/sinewave
+x = np.arange(0, 10, 1)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.show()
